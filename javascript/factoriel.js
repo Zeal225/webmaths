84c85fee364eae57bf5e54a939591c1b425000db
oraclerevolution <mac.n.drix@gmail.com>
2018-01-29 11:32:08 +0100

    update factoriel.js

--- javascript/factoriel.js
+++ javascript/factoriel.js
@@ -1,14 +1,17 @@
 'use strict'
 
-const factoriel = (e) => {
-    if(e==0 || e==1){
-       return 1
-    }
-    let fact = 1
-    for (let index = 2; index < e+1 ; index++) {
-        fact = fact*index
-    }
-    return fact
-}
-
-console.log(factoriel(5)) //le resultat doit etre 120 normalement
+//const factoriel = (e) => {
+//    if(e==0 || e==1){
+//    return 1
+//    }
+//    let fact = 1
+//    for (let index = 2; index < e+1 ; index++) {
+//        fact = fact*index
+//    }
+//    return fact
+//}
+let valeur  = document.getElementById('espace_calcul').value
+let bouton = document.getElementById('bouton')
+bouton.addEventListener('click',factoriel = () => {
+document.getElementById('espace_resultat').appendChild(document.createTextNode('vous venez de cliquer sur le bouton'))
+})
