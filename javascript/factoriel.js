e3588b7d68cfbf63ea3eae998b5e17bbd31f4a1a
DivinityAdvice <advicedivinity18@gmail.com>
2018-02-05 22:42:43 +0100

    modifs factoriel

--- javascript/factoriel.js
+++ javascript/factoriel.js
@@ -1,17 +1,12 @@
 'use strict'
 
-//const factoriel = (e) => {
-//    if(e==0 || e==1){
-//    return 1
-//    }
-//    let fact = 1
-//    for (let index = 2; index < e+1 ; index++) {
-//        fact = fact*index
-//    }
-//    return fact
-//}
-let valeur  = document.getElementById('espace_calcul').value
-let bouton = document.getElementById('bouton')
-bouton.addEventListener('click',factoriel = () => {
-document.getElementById('espace_resultat').appendChild(document.createTextNode('vous venez de cliquer sur le bouton'))
-})
+const factoriel = (e) => {
+    if(e==0 || e==1){
+    return 1
+    }
+   let fact = 1
+    for (let index = 2; index < e+1 ; index++) {
+        fact = fact*index
+    }
+    return fact
+}
